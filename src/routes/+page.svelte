b378dd9e8a5558b35960d49fb85195e7020a807c
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-11-24 10:48:10 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+
+<svg viewbox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" stroke-linejoin="round" stroke-linecap="round" stroke-width="1">
+    <rect x="0" y="0" width="1000" height="1000" fill="white" stroke="none"/>
+
+    
+
+</svg>
+
+
+<style>
+  /* make sure the SVG does not make the container larger than intended */
+  svg {
+    max-width: 100%;
+    max-height: 100%;
+    display: block;
+    padding: 20px;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
