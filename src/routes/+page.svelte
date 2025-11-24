0473d204c8d503abdfbb2ad4ba245016ed2de587
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2025-11-24 14:37:24 +0100

    first tries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,28 @@
 </script>
 
 
-<svg viewbox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" stroke-linejoin="round" stroke-linecap="round" stroke-width="1">
-    <rect x="0" y="0" width="1000" height="1000" fill="white" stroke="none"/>
+<svg viewbox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg" stroke-linejoin="round" stroke-linecap="round" stroke-width="1" fill="none" stroke="#000000">
+    <rect x="0" y="0" width="1000" height="1000" fill="#ffffff" stroke="none"/>
 
-    
+    <!-- <polygon points="100 20, 180 60, 180 150, 100 190, 20 150, 20 60" />
+    <polygon points="260 20, 340 60, 340 150, 260 190, 180 150, 180 60" /> -->
+
+     <polygon transform="translate(50 100) rotate(20) scale(0.5 1)" points="0 -60, 50 -30, 50 30, 0 60, -50 30, -50 -30" />
+     <polygon transform="translate(150 100)" points="0 -60, 50 -30, 50 30, 0 60, -50 30, -50 -30" />
+     <polygon transform="translate(250 100)" points="0 -60, 50 -30, 50 30, 0 60, -50 30, -50 -30" />
+
+
+    <!-- <g stroke="#000000" stroke-width="2">
+      <circle cx="100" cy="100" r="80" fill="#ffcc00"  />
+      <ellipse cx="300" cy="100" rx="80" ry="40" fill="#00ccff"  />
+      <rect x="450" y="20" width="160" height="160" fill="#cc00ff"  />
+      <polygon points="700 20, 780 180, 620 180" fill="#00ff00" stroke-width="20" />
+      <polyline points="870 20, 950 180, 800 180" fill="#66aaff" stroke="#ff0000" />
+      <line x1="100" y1="300" x2="300" y2="400" stroke="#0000ff" stroke-width="40"/>
+      <path d="M450 300 C 500 200, 600 400, 700 300 S 800 400, 850 300" fill="none" stroke="#ff00ff" stroke-width="4"/>
+  </g> -->
+
+
 
 </svg>
 
